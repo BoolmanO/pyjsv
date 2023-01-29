f3a2975fddae5c2c9dc7394ef3c760747c00a068
BoolmanO <ponos585858@gmail.com>
2023-01-29 18:43:06 +0600

    update readme

--- .dev.py
+++
@@ -1,6 +0,0 @@
-import requests
-from pyjsv import SimpleJson, SimpleXml
-
-url = "https://iss.moex.com/iss/history/engines/stock/markets/index/securities.xml?date=2022.12.12"
-my_data = SimpleXml.upload_from_str(requests.get(url).text)
-print(my_data.get_dict())
